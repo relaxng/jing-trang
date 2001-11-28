4fd9eebede38e1a8540fed1f2fffe56eee5a7997
nobody <nobody@369101cc-9a96-11dd-8e58-870c635edf7a>
2001-11-28 10:17:31 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_0'.

--- src/com/thaiopensource/relaxng/util/ExegenDriver.java
+++
@@ -1,9 +0,0 @@
-package com.thaiopensource.relaxng.util;
-
-class ExegenDriver {
-  static public void main(String[] args) {
-    Driver.setUsageKey("exegen_usage");
-    Driver.setParser("com.jclark.xml.sax.Driver", false);
-    Driver.main(args);
-  }
-}
