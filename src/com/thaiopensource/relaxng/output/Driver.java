4950acc2ba85615916448e7ac858fb249200d1a6
James Clark <jjc@jclark.com>
2002-02-27 11:38:12 +0000

    Use NonXmlParseable Make first argument a file

--- src/com/thaiopensource/relaxng/output/Driver.java
+++ src/com/thaiopensource/relaxng/output/Driver.java
@@ -1,11 +1,13 @@
 package com.thaiopensource.relaxng.output;
 
 import com.thaiopensource.relaxng.parse.sax.SAXParseable;
+import com.thaiopensource.relaxng.parse.nonxml.NonXmlParseable;
 import com.thaiopensource.relaxng.edit.SchemaCollection;
 import com.thaiopensource.relaxng.edit.Pattern;
 import com.thaiopensource.relaxng.edit.SchemaBuilderImpl;
 import com.thaiopensource.relaxng.util.Jaxp11XMLReaderCreator;
 import com.thaiopensource.relaxng.util.DraconianErrorHandler;
+import com.thaiopensource.relaxng.util.ValidationEngine;
 import com.thaiopensource.relaxng.IncorrectSchemaException;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -16,9 +18,8 @@ import java.io.IOException;
 public class Driver {
   static public void main(String[] args) throws IncorrectSchemaException, SAXException, IOException {
     SchemaCollection sc = new SchemaCollection();
-    Pattern p = SchemaBuilderImpl.parse(new SAXParseable(new Jaxp11XMLReaderCreator(),
-                                                         new InputSource(args[0]),
-                                                         new DraconianErrorHandler()),
+    Pattern p = SchemaBuilderImpl.parse(new NonXmlParseable(ValidationEngine.fileInputSource(args[0]),
+                                                            new DraconianErrorHandler()),
                                         sc,
                                         new DatatypeLibraryLoader());
     XmlOutput.output(p, args[1]);
