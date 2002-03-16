54403dc8f8075a94a937ab184e2925769ccafcfe
jjc <jjc@369101cc-9a96-11dd-8e58-870c635edf7a>
2002-03-16 03:09:26 +0000

    Proper error formatting for convert driver

--- branches/multiparse/src/com/thaiopensource/relaxng/output/Driver.java
+++ branches/multiparse/src/com/thaiopensource/relaxng/output/Driver.java
@@ -11,45 +11,68 @@ import com.thaiopensource.relaxng.parse.nonxml.NonXmlParseable;
 import com.thaiopensource.relaxng.util.DraconianErrorHandler;
 import com.thaiopensource.relaxng.util.Jaxp11XMLReaderCreator;
 import com.thaiopensource.relaxng.util.ValidationEngine;
+import com.thaiopensource.relaxng.util.ErrorHandlerImpl;
+import com.thaiopensource.util.Localizer;
 import org.relaxng.datatype.helpers.DatatypeLibraryLoader;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXParseException;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Driver {
+  static private Localizer localizer = new Localizer(Driver.class);
   static public void main(String[] args) throws IncorrectSchemaException, SAXException, IOException {
     System.exit(doMain(args));
   }
 
   static public int doMain(String[] args) throws IncorrectSchemaException, SAXException, IOException {
-    ErrorHandler eh = new DraconianErrorHandler();
+    ErrorHandlerImpl eh = new ErrorHandlerImpl();
+    try {
+      InputSource in = ValidationEngine.fileInputSource(args[0]);
+      Parseable parseable;
+      String ext = extension(args[0]);
+      if (ext.equalsIgnoreCase(".rng"))
+        parseable = new SAXParseable(new Jaxp11XMLReaderCreator(), in, eh);
+      else if (ext.equalsIgnoreCase(".rngnx") || ext.equalsIgnoreCase(".rnx"))
+        parseable = new NonXmlParseable(in, eh);
+      else {
+        eh.printException(new SAXException(localizer.message("unrecognized_input_extension", ext)));
+        return 2;
+      }
+      OutputFormat of;
+      ext = extension(args[1]);
+      if (ext.equalsIgnoreCase(".dtd"))
+        of = new DtdOutputFormat();
+      else if (ext.equalsIgnoreCase(".rng"))
+        of = new RngOutputFormat();
+      else {
+        eh.printException(new SAXException(localizer.message("unrecognized_output_extension", ext)));
+        return 2;
+      }
+      SchemaCollection sc = SchemaBuilderImpl.parse(parseable,
+                                                    new DatatypeLibraryLoader());
+      OutputDirectory od = new LocalOutputDirectory(new File(args[1]));
+      of.output(sc, od, eh);
+      return 0;
+    }
+    catch (IncorrectSchemaException e) {
+    }
+    catch (IOException e) {
+      eh.printException(e);
+    }
+    catch (SAXException e) {
+      eh.printException(e);
+    }
+    return 1;
+  }
 
-    InputSource in = ValidationEngine.fileInputSource(args[0]);
-    Parseable parseable;
-    if (args[0].endsWith(".rng"))
-      parseable = new SAXParseable(new Jaxp11XMLReaderCreator(), in, eh);
-    else if (args[0].endsWith(".rngnx"))
-      parseable = new NonXmlParseable(in, eh);
-    else {
-      System.err.println("unrecognized input file extension");
-      return 2;
-    }
-    OutputFormat of;
-    if (args[1].endsWith(".dtd"))
-      of = new DtdOutputFormat();
-    else if (args[1].endsWith(".rng"))
-      of = new RngOutputFormat();
-    else {
-      System.err.println("unrecognized output file extension");
-      return 2;
-    }
-    SchemaCollection sc = SchemaBuilderImpl.parse(parseable,
-                                                  new DatatypeLibraryLoader());
-    OutputDirectory od = new LocalOutputDirectory(new File(args[1]));
-    of.output(sc, od, eh);
-    return 0;
+  static private String extension(String s) {
+    int dot = s.lastIndexOf(".");
+    if (dot < 0)
+      return "";
+    return s.substring(dot);
   }
 }
