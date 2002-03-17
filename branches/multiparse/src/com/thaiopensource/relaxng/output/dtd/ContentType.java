efc9829b63f04799340af2c5813dfc1a42635757
jjc <jjc@369101cc-9a96-11dd-8e58-870c635edf7a>
2002-03-17 04:53:34 +0000

    Split Type into ContentType and AttributeType

---
+++ branches/multiparse/src/com/thaiopensource/relaxng/output/dtd/ContentType.java
@@ -0,0 +1,165 @@
+package com.thaiopensource.relaxng.output.dtd;
+
+class ContentType {
+  private final ContentType parent1;
+  private final ContentType parent2;
+  static ContentType COMPLEX_TYPE = new ContentType();
+  static ContentType MIXED_ELEMENT_CLASS = new ContentType();
+  static ContentType NOT_ALLOWED = new ContentType();
+  static ContentType SIMPLE_TYPE = new ContentType();
+  static ContentType EMPTY = new ContentType(COMPLEX_TYPE);
+  static ContentType TEXT = new ContentType(MIXED_ELEMENT_CLASS, COMPLEX_TYPE);
+  static ContentType DIRECT_TEXT = new ContentType(TEXT);
+  static ContentType MIXED_MODEL = new ContentType(COMPLEX_TYPE);
+  static ContentType MODEL_GROUP = new ContentType(COMPLEX_TYPE);
+  static ContentType ELEMENT_CLASS = new ContentType(MODEL_GROUP);
+  static ContentType DIRECT_MULTI_ELEMENT = new ContentType(ELEMENT_CLASS);
+  static ContentType DIRECT_SINGLE_ELEMENT = new ContentType(DIRECT_MULTI_ELEMENT);
+  static ContentType ZERO_OR_MORE_ELEMENT_CLASS = new ContentType(MODEL_GROUP);
+  static ContentType ENUM = new ContentType(SIMPLE_TYPE);
+  static ContentType ERROR = new ContentType();
+
+  private ContentType() {
+    this.parent1 = null;
+    this.parent2 = null;
+  }
+
+  private ContentType(ContentType parent1) {
+    this.parent1 = parent1;
+    this.parent2 = null;
+  }
+
+  private ContentType(ContentType parent1, ContentType parent2) {
+    this.parent1 = parent1;
+    this.parent2 = parent2;
+  }
+
+  boolean isA(ContentType t) {
+    if (this == t)
+      return true;
+    if (parent1 != null && parent1.isA(t))
+      return true;
+    if (parent2 != null && parent2.isA(t))
+      return true;
+    return false;
+  }
+
+  static ContentType zeroOrMore(ContentType t) {
+    if (t.isA(ELEMENT_CLASS))
+      return ZERO_OR_MORE_ELEMENT_CLASS;
+    if (t.isA(MIXED_ELEMENT_CLASS))
+      return MIXED_MODEL;
+    return oneOrMore(t);
+  }
+
+  static ContentType oneOrMore(ContentType t) {
+    if (t == ERROR)
+      return ERROR;
+    if (t == EMPTY)
+      return EMPTY;
+    if (t.isA(MODEL_GROUP))
+      return MODEL_GROUP;
+    return null;
+  }
+
+  static ContentType group(ContentType t1, ContentType t2) {
+    if (t1 == ERROR || t2 == ERROR)
+      return ERROR;
+    if (t1.isA(MODEL_GROUP) && t2.isA(MODEL_GROUP))
+      return MODEL_GROUP;
+    if (t1.isA(EMPTY) && t2.isA(EMPTY))
+      return EMPTY;
+    if (t1.isA(EMPTY)) {
+      if (t2.isA(ZERO_OR_MORE_ELEMENT_CLASS))
+        return ZERO_OR_MORE_ELEMENT_CLASS;
+      if (t2.isA(MODEL_GROUP))
+        return MODEL_GROUP;
+      if (t2.isA(COMPLEX_TYPE))
+        return COMPLEX_TYPE;
+    }
+    else if (t2.isA(EMPTY))
+      return group(t1, t2);
+    return null;
+  }
+
+  static ContentType mixed(ContentType t) {
+    if (t.isA(EMPTY))
+      return COMPLEX_TYPE; // XXX should be TEXT?
+    if (t.isA(ZERO_OR_MORE_ELEMENT_CLASS))
+      return MIXED_MODEL;
+    return null;
+  }
+
+  static ContentType interleave(ContentType t1, ContentType t2) {
+    if (t1 == ERROR || t2 == ERROR)
+      return ERROR;
+    if (t1.isA(EMPTY) && t2.isA(EMPTY))
+      return EMPTY;
+    if (t1.isA(EMPTY)) {
+      if (t2.isA(ZERO_OR_MORE_ELEMENT_CLASS))
+        return ZERO_OR_MORE_ELEMENT_CLASS;
+      if (t2.isA(MODEL_GROUP))
+        return MODEL_GROUP;
+      if (t2.isA(COMPLEX_TYPE))
+        return COMPLEX_TYPE;
+    }
+    else if (t2.isA(EMPTY))
+      return interleave(t1, t2);
+    return null;
+  }
+
+  static ContentType optional(ContentType t) {
+    if (t == ERROR)
+      return ERROR;
+    if (t == EMPTY)
+      return EMPTY;
+    if (t.isA(MODEL_GROUP))
+      return MODEL_GROUP;
+    if (t.isA(MIXED_ELEMENT_CLASS))
+      return MIXED_ELEMENT_CLASS;
+    if (t == NOT_ALLOWED)
+      return MODEL_GROUP;
+    return null;
+  }
+
+  static ContentType choice(ContentType t1, ContentType t2) {
+    if (t1 == ERROR || t2 == ERROR)
+      return ERROR;
+    // XXX what about EMPTY?
+    if (t1 == NOT_ALLOWED) {
+      if (t2 == NOT_ALLOWED)
+        return NOT_ALLOWED;
+      if (t2.isA(ELEMENT_CLASS))
+        return ELEMENT_CLASS;
+      if (t2.isA(MIXED_ELEMENT_CLASS))
+        return MIXED_ELEMENT_CLASS;
+      if (t2.isA(MODEL_GROUP))
+        return MODEL_GROUP;
+      if (t2.isA(ENUM))
+        return ENUM;
+      return null;
+    }
+    if (t2 == NOT_ALLOWED)
+      return choice(t2, t1);
+    if (t1.isA(ELEMENT_CLASS) && t2.isA(ELEMENT_CLASS))
+      return ELEMENT_CLASS;
+    if (t1.isA(MODEL_GROUP) && t2.isA(MODEL_GROUP))
+      return MODEL_GROUP;
+    if ((t1.isA(MIXED_ELEMENT_CLASS) && t2.isA(ELEMENT_CLASS))
+            || (t1.isA(ELEMENT_CLASS) && t2.isA(MIXED_ELEMENT_CLASS)))
+      return MIXED_ELEMENT_CLASS;
+    if (t1.isA(ENUM) && t2.isA(ENUM))
+      return ENUM;
+    return null;
+  }
+
+  static ContentType ref(ContentType t) {
+    if (t == DIRECT_TEXT)
+      return TEXT;
+    if (t.isA(DIRECT_MULTI_ELEMENT))
+      return ELEMENT_CLASS;
+    if (t == ZERO_OR_MORE_ELEMENT_CLASS)
+      return MODEL_GROUP;
+    return t;
+  }
+}
