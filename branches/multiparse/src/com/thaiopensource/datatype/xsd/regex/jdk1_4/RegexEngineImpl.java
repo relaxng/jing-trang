f1a42543d8089269e7ad68b4ec860b81ce01fcb5
jjc <jjc@369101cc-9a96-11dd-8e58-870c635edf7a>
2002-04-07 05:26:18 +0000

    Don't catch PatternSyntaxException

--- branches/multiparse/src/com/thaiopensource/datatype/xsd/regex/jdk1_4/RegexEngineImpl.java
+++ branches/multiparse/src/com/thaiopensource/datatype/xsd/regex/jdk1_4/RegexEngineImpl.java
@@ -1,7 +1,6 @@
 package com.thaiopensource.datatype.xsd.regex.jdk1_4;
 
 import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 import java.util.regex.Matcher;
 
 import com.thaiopensource.datatype.xsd.RegexEngine;
@@ -11,24 +10,17 @@ import com.thaiopensource.datatype.xsd.InvalidRegexException;
 public class RegexEngineImpl implements RegexEngine {
   public RegexEngineImpl() {
     // Force a linkage error on instantiation if JDK 1.4 is not available.
-    try {
-      Pattern.compile("");
-    }
-    catch (PatternSyntaxException e) { }
+    Pattern.compile("x");
   }
 
   public Regex compile(String str) throws InvalidRegexException {
-    try {
-      final Pattern pattern = Pattern.compile(Translator.translate(str));
-      return new Regex() {
-        public boolean matches(String str) {
-          return pattern.matcher(str).matches();
-        }
-      };
-    }
-    catch (PatternSyntaxException e) {
-      // don't use getIndex() since that returns the index in the translated regex not the original regex
-      throw new InvalidRegexException(e.getDescription());
-    }
+    // Don't catch PatternSyntaxException
+    // The Translator should detect all syntax errors
+    final Pattern pattern = Pattern.compile(Translator.translate(str));
+    return new Regex() {
+      public boolean matches(String str) {
+        return pattern.matcher(str).matches();
+      }
+    };
   }
 }
