8c14ade3200fcc75edb37abc7eedf46378f9ddd4
James Clark <jjc@jclark.com>
2002-04-23 08:25:31 +0000

    Does not need to be public any more

--- src/com/thaiopensource/datatype/xsd/NCNameDatatype.java
+++ src/com/thaiopensource/datatype/xsd/NCNameDatatype.java
@@ -2,7 +2,7 @@ package com.thaiopensource.datatype.xsd;
 
 import com.thaiopensource.xml.util.Naming;
 
-public class NCNameDatatype extends NameDatatype {
+class NCNameDatatype extends NameDatatype {
   public boolean lexicallyAllows(String str) {
     return Naming.isNcname(str);
   }
