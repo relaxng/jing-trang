af3d241b817cbe513f2a5246c0995ad9c9aeb8cb
James Clark <jjc@jclark.com>
2002-04-24 10:37:06 +0000

    Implement remaining date/time datatypes

---
+++ src/com/thaiopensource/datatype/xsd/DurationDatatype.java
@@ -0,0 +1,18 @@
+package com.thaiopensource.datatype.xsd;
+
+class DurationDatatype extends RegexDatatype {
+  static private final String PATTERN =
+    "-?P([0-9]+Y)?([0-9]+M)?([0-9]+D)?T?([0-9]+H)?([0-9]+M)?(([0-9]+(\\.[0-9]*)?|\\.[0-9]+)S)?";
+  DurationDatatype() {
+    super(PATTERN);
+  }
+
+  public boolean lexicallyAllows(String str) {
+    if (!super.lexicallyAllows(str))
+      return false;
+    char last = str.charAt(str.length()-1);
+    // This enforces that there must be at least one component
+    // and that T is omitted if all time components are omitted
+    return last != 'P' && last != 'T';
+  }
+}
