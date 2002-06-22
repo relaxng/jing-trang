361379014e4051e5ca343ff4a831b14beebe979f
jjc <jjc@369101cc-9a96-11dd-8e58-870c635edf7a>
2002-06-22 03:08:13 +0000

    Add hashCode/equals/EXACTLY_ONE

--- branches/XSD/trang/src/com/thaiopensource/relaxng/output/xsd/basic/Occurs.java
+++ branches/XSD/trang/src/com/thaiopensource/relaxng/output/xsd/basic/Occurs.java
@@ -4,6 +4,7 @@ public class Occurs {
   private final int min;
   private final int max;
   static public final int UNBOUNDED = Integer.MAX_VALUE;
+  static public final Occurs EXACTLY_ONE = new Occurs(1, 1);
   static public final Occurs ONE_OR_MORE = new Occurs(1, UNBOUNDED);
   static public final Occurs ZERO_OR_MORE = new Occurs(0, UNBOUNDED);
   static public final Occurs OPTIONAL = new Occurs(0, 1);
@@ -19,6 +20,16 @@ public class Occurs {
 
   public int getMax() {
     return max;
+  }
+
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Occurs))
+      return false;
+    return this.min == ((Occurs)obj).min && max == ((Occurs)obj).max;
+  }
+
+  public int hashCode() {
+    return min ^ max;
   }
 
   static public Occurs add(Occurs occ1, Occurs occ2) {
