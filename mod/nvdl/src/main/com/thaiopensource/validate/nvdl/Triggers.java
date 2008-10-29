92219abf9ebc31a19f3def40d2095f6f144e4c91
George Bina <george@oxygenxml.com>
2008-10-29 19:08:19 +0000

    Work on issue 24. NVDL adds a concept of trigger so we need support for that.

---
+++ mod/nvdl/src/main/com/thaiopensource/validate/nvdl/Triggers.java
@@ -0,0 +1,45 @@
+package com.thaiopensource.validate.nvdl;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+class Triggers {
+  
+  /**
+   * A list with defined triggers.
+   */
+  private final List triggers = new ArrayList();
+    
+  /**
+   * Adds a trigger for a namespace and a local name.
+   * @param ns The namespace.
+   * @param nameList The list of local names.
+   */
+  public String addTrigger(String ns, String nameList) {
+	Trigger t = new Trigger(ns, nameList);
+	triggers.add(t);
+	return t.errors;
+  }
+  
+  /**
+   * Indicates if we have a trigger on a namespace and local name.
+   * @param ns The namespace.
+   * @param name The local name.
+   * @return true if we have a trigger set, otherwise false.
+   */
+  public boolean trigger(String namespace, String name, String parent) {
+    // iterate triggers
+	Iterator i = triggers.iterator();
+	while (i.hasNext()) {
+	  Trigger t = (Trigger)i.next();
+	  if (t.trigger(namespace, name, parent)) {
+	    return true;
+	  }
+	}
+	return false;
+  }
+  
+}
