36236f947f55953befffa045aee5bcafb834b109
jjc@jclark.com <jjc@jclark.com@369101cc-9a96-11dd-8e58-870c635edf7a>
2008-11-03 01:47:34 +0000

    Style fixes to nvdl.NamespaceSpecification.

--- branches/nvdl/mod/nvdl/src/main/com/thaiopensource/validate/nvdl/NamespaceSpecification.java
+++ branches/nvdl/mod/nvdl/src/main/com/thaiopensource/validate/nvdl/NamespaceSpecification.java
@@ -21,8 +21,7 @@ class NamespaceSpecification {
    * Constant for any namespace.
    */
   static final String ANY_NAMESPACE = "##any";
-  
-  
+
   /**
    * The namespace pattern, may contain one or more occurances of the wildcard.
    */
@@ -32,8 +31,7 @@ class NamespaceSpecification {
    * The wildcard character, by default it is *.
    */
   String wildcard = DEFAULT_WILDCARD;
-  
-  
+    
   /**
    * Creates a namespace specification from a namespace pattern
    * using the default wildcard, that is *.
@@ -57,55 +55,64 @@ class NamespaceSpecification {
   /**
    * Check if this namespace specification competes with 
    * another namespace specification.
-   * @param n The namespace specification we need to check if 
+   * @param other The namespace specification we need to check if
    * it competes with this namespace specification.
    * @return true if the namespace specifications compete.
    */
-  public boolean compete(NamespaceSpecification n) {
-    // if no wildcard for n then we check coverage
-    if ("".equals(n.wildcard)) {
-      return covers(n.ns);
+  public boolean compete(NamespaceSpecification other) {
+    // if no wildcard for other then we check coverage
+    if ("".equals(other.wildcard)) {
+      return covers(other.ns);
     }
     // split the namespaces at wildcards     
-    String[] nParts = split(n.ns, n.wildcard);
+    String[] otherParts = split(other.ns, other.wildcard);
     
     // if the given namepsace specification does not use its wildcard
     // then we just look if the current namespace specification covers it
-    if (nParts.length ==1) {
-      return covers(n.ns);
+    if (otherParts.length == 1) {
+      return covers(other.ns);
     }
     // if no wildcard for the current namespace specification
     if ("".equals(wildcard)) {
-      return n.covers(ns);
+      return other.covers(ns);
     }
     // also for the current namespace specification
     String[] parts = split(ns, wildcard); 
     // now check if the current namespace specification is just an URI
-    if (parts.length ==1) {
-      return n.covers(ns);
+    if (parts.length == 1) {
+      return other.covers(ns);
     }
     // now each namespace specification contains wildcards
     // suppose we have 
     // ns   = a1*a2*...*an 
     // and 
-    // n.ns = b1*b2*...*bm
-    // then we only need to check match(a1, b1) and match (an, bn) where
-    // match(a,b) in this case means a starts with b or b starts with a. 
-    return 
-    (parts[0].startsWith(nParts[0]) || nParts[0].startsWith(parts[0]))&&
-    (   parts[parts.length-1].startsWith(nParts[nParts.length-1]) || 
-        nParts[nParts.length-1].startsWith(parts[parts.length-1])
-     );   
+    // other.ns = b1*b2*...*bm
+    // then we only need to check matchPrefix(a1, b1) and matchPrefix(an, bn) where
+    // matchPrefix(a, b) means a starts with b or b starts with a.
+    return matchPrefix(parts[0], otherParts[0])
+           || matchPrefix(parts[parts.length - 1], otherParts[otherParts.length - 1]);
+  }
+
+  /**
+   * Checks with either of the strings starts with the other.
+   * @param s1 a String
+   * @param s2 a String
+   * @return true if s1 starts with s2 or s2 starts with s1, false otherwise
+   */
+  static private boolean matchPrefix(String s1, String s2) {
+    return s1.startsWith(s2) || s2.startsWith(s1);
   }
 
   private String[] split(String value, String regexp) {
-    String[] parts = null;
+    String[] parts;
     try {
       parts = value.split("\\" + regexp, -1);
-    } catch (PatternSyntaxException e) {
+    }
+    catch (PatternSyntaxException e) {
       try {
         parts = value.split(regexp, -1);
-      } catch (PatternSyntaxException e2) {
+      }
+      catch (PatternSyntaxException e2) {
         parts = new String[]{value};
       }
     }
@@ -124,8 +131,6 @@ class NamespaceSpecification {
     if (ANY_NAMESPACE.equals(ns) || "".equals(wildcard)) {
       return ns.equals(uri);
     }
-    
-    
     String[] parts = split(ns, wildcard);    
     // no wildcard
     if (parts.length == 1) {
@@ -136,18 +141,18 @@ class NamespaceSpecification {
     if (!uri.startsWith(parts[0])) {
       return false;
     }
-    if (!uri.endsWith(parts[parts.length-1])) {
+    if (!uri.endsWith(parts[parts.length - 1])) {
       return false;
     }
     // Check that all remaining parts match the remaining URI.
     int start = parts[0].length();
-    int end = uri.length() - parts[parts.length-1].length();
-    for (int i=1; i<parts.length-1; i++) {
+    int end = uri.length() - parts[parts.length - 1].length();
+    for (int i = 1; i < parts.length - 1; i++) {
       if (start > end) {
         return false;
       }
       int match = uri.indexOf(parts[i], start);
-      if (match==-1 || match+parts[i].length()>end) {
+      if (match == -1 || match + parts[i].length() > end) {
         return false;
       }
       start = match + parts[i].length();
@@ -170,6 +175,6 @@ class NamespaceSpecification {
    * Get a hashcode for this namespace specification.
    */
   public int hashCode() {
-    return (wildcard+"|"+ns).hashCode();
+    return (wildcard + "|" + ns).hashCode();
   }
 }
