03f00b94d874f4d69f5ab6acf937db17db1c840d
jjc@jclark.com <jjc@jclark.com@369101cc-9a96-11dd-8e58-870c635edf7a>
2008-11-08 11:29:08 +0000

    Rename com.thaiopensource.relaxng.javax to jaxp.

--- trunk/mod/rng-validate/src/main/com/thaiopensource/relaxng/jaxp/Resolver.java
+++ trunk/mod/rng-validate/src/main/com/thaiopensource/relaxng/jaxp/Resolver.java
@@ -1,4 +1,4 @@
-package com.thaiopensource.relaxng.javax;
+package com.thaiopensource.relaxng.jaxp;
 
 import com.thaiopensource.xml.sax.BasicResolver;
 import com.thaiopensource.xml.util.WellKnownNamespaces;
