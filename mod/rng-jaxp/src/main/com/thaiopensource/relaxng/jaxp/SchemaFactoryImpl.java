fe0a802e5294c4fec6f4d0f5af92debce560f73e
James Clark <jjc@jclark.com>
2008-11-16 03:27:37 +0000

    Implement property for DatatypeLibraryFactory in JAXP interface.

--- mod/rng-jaxp/src/main/com/thaiopensource/relaxng/jaxp/SchemaFactoryImpl.java
+++ mod/rng-jaxp/src/main/com/thaiopensource/relaxng/jaxp/SchemaFactoryImpl.java
@@ -13,6 +13,8 @@ import org.relaxng.datatype.DatatypeLibraryFactory;
 import org.w3c.dom.ls.LSResourceResolver;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 import javax.xml.transform.Source;
 import javax.xml.validation.Schema;
@@ -22,7 +24,11 @@ import java.io.IOException;
 public abstract class SchemaFactoryImpl extends SchemaFactory {
   private ErrorHandler errorHandler = null;
   private LSResourceResolver resourceResolver = null;
-  private DatatypeLibraryFactory dlf = null;
+  private DatatypeLibraryFactory datatypeLibraryFactory = null;
+  /* If this is true, then logically datatypeLibraryFactory is an instance of DatatypeLibraryLoader,
+     but we create it lazily, so that we don't need to create it if the user specifies their own. */
+  private boolean defaultDatatypeLibraryFactory = true;
+  static final public String PROPERTY_DATATYPE_LIBRARY_FACTORY = "http://relaxng.org/properties/datatype-library-factory";
 
   protected SchemaFactoryImpl() {
   }
@@ -58,13 +64,10 @@ public abstract class SchemaFactoryImpl extends SchemaFactory {
     ErrorHandler eh = errorHandler;
     if (eh == null)
       eh = new DraconianErrorHandler();
-    // XXX add a property to enable user to set this
-    if (dlf == null)
-      dlf = new DatatypeLibraryLoader();
     Parseable parseable = createParseable(source, saxResolver, eh);
     SchemaPatternBuilder spb = new SchemaPatternBuilder();
     try {
-      return new SchemaImpl(spb, SchemaBuilderImpl.parse(parseable, eh, dlf, spb, false));
+      return new SchemaImpl(spb, SchemaBuilderImpl.parse(parseable, eh, getDatatypeLibraryFactory(), spb, false));
     }
     catch (IOException e) {
       throw new SAXException(e);
@@ -73,6 +76,34 @@ public abstract class SchemaFactoryImpl extends SchemaFactory {
       // XXX not sure what we're supposed to do here
       throw new SAXException("invalid schema");
     }
+  }
+
+  public void setProperty(String name, Object object) throws SAXNotRecognizedException, SAXNotSupportedException {
+    if (PROPERTY_DATATYPE_LIBRARY_FACTORY.equals(name)) {
+      if (object instanceof DatatypeLibraryFactory) {
+        datatypeLibraryFactory = (DatatypeLibraryFactory)object;
+        defaultDatatypeLibraryFactory = false;
+      }
+      else
+        throw new SAXNotSupportedException("value of \"" + PROPERTY_DATATYPE_LIBRARY_FACTORY +
+                                           "\" property does not implement org.relaxng.datatype.DatatypeLibraryFactory");
+    }
+    else
+      super.setProperty(name, object);
+  }
+
+  public Object getProperty(String name) throws SAXNotRecognizedException, SAXNotSupportedException {
+    if (PROPERTY_DATATYPE_LIBRARY_FACTORY.equals(name))
+      return getDatatypeLibraryFactory();
+    return super.getProperty(name);
+  }
+
+  private DatatypeLibraryFactory getDatatypeLibraryFactory() {
+    if (defaultDatatypeLibraryFactory) {
+      datatypeLibraryFactory = new DatatypeLibraryLoader();
+      defaultDatatypeLibraryFactory = false;
+    }
+    return datatypeLibraryFactory;
   }
 
   abstract protected Parseable createParseable(Source source, SAXResolver resolver, ErrorHandler eh) throws SAXException;
