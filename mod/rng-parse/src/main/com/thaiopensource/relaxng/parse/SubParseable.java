5bcdb9778926831ac596362c9ca35e72c131cc9b
James Clark <jjc@jclark.com>
2008-11-27 01:46:19 +0000

    SubParseable.getUri() may return null.

--- mod/rng-parse/src/main/com/thaiopensource/relaxng/parse/SubParseable.java
+++ mod/rng-parse/src/main/com/thaiopensource/relaxng/parse/SubParseable.java
@@ -3,6 +3,6 @@ package com.thaiopensource.relaxng.parse;
 public interface SubParseable extends Parseable {
   ParsedPattern parseAsInclude(SchemaBuilder f, IncludedGrammar g)
           throws BuildException, IllegalSchemaException;
-  /* The returned URI will have disallowed characters escaped. */
+  /* The returned URI will have disallowed characters escaped. May return null for top-level schema. */
   String getUri();
 }
