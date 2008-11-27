6d80c1b925b47e4ab4ea8fe50f9b11e70e8e3b2b
James Clark <jjc@jclark.com>
2008-11-27 04:11:49 +0000

    Add some more javadoc comments.

--- mod/jaxp/src/main/com/thaiopensource/validation/Schema2.java
+++ mod/jaxp/src/main/com/thaiopensource/validation/Schema2.java
@@ -3,7 +3,10 @@ package com.thaiopensource.validation;
 import javax.xml.validation.Schema;
 
 /**
- *
+ * An extension to the Schema abstract class.  The main difference is that
+ * there is a default implementation of newValidator in terms of newValidatorHandler.
+ * Also both newValidator and newValidatorHandler return the extended versions
+ * of Validator and ValidatorHandler (using covariant return types).
  */
 public abstract class Schema2 extends Schema {
   protected Schema2() { }
