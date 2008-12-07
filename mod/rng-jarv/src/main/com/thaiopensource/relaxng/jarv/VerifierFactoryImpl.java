ba4c7ba8cd02d4d9ceb913c9e7cc996da21e488b
James Clark <jjc@jclark.com>
2008-12-07 06:12:28 +0000

    Generify VerifierFactoryImpl.

--- mod/rng-jarv/src/main/com/thaiopensource/relaxng/jarv/VerifierFactoryImpl.java
+++ mod/rng-jarv/src/main/com/thaiopensource/relaxng/jarv/VerifierFactoryImpl.java
@@ -6,10 +6,15 @@ import com.thaiopensource.relaxng.parse.Parseable;
 import com.thaiopensource.relaxng.parse.sax.SAXParseable;
 import com.thaiopensource.relaxng.pattern.SchemaBuilderImpl;
 import com.thaiopensource.relaxng.pattern.SchemaPatternBuilder;
+import com.thaiopensource.relaxng.pattern.Pattern;
+import com.thaiopensource.relaxng.pattern.NameClass;
+import com.thaiopensource.relaxng.pattern.CommentListImpl;
+import com.thaiopensource.relaxng.pattern.AnnotationsImpl;
 import com.thaiopensource.resolver.Resolver;
 import com.thaiopensource.resolver.xml.sax.SAX;
 import com.thaiopensource.resolver.xml.sax.SAXResolver;
 import com.thaiopensource.xml.sax.DraconianErrorHandler;
+import com.thaiopensource.util.VoidValue;
 import org.iso_relax.verifier.Schema;
 import org.iso_relax.verifier.VerifierFactory;
 import org.relaxng.datatype.DatatypeLibraryFactory;
@@ -17,6 +22,7 @@ import org.xml.sax.EntityResolver;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.Locator;
 
 import javax.xml.transform.sax.SAXSource;
 import java.io.IOException;
@@ -34,9 +40,10 @@ public class VerifierFactoryImpl extends VerifierFactory {
     if (entityResolver != null)
       resolver = SAX.createResolver(entityResolver, true);
     SAXResolver saxResolver = new SAXResolver(resolver);
-    Parseable parseable = new SAXParseable(new SAXSource(saxResolver.createXMLReader(), inputSource),
-                                           saxResolver,
-                                           eh);
+    Parseable<Pattern, NameClass, Locator, VoidValue, CommentListImpl, AnnotationsImpl> parseable
+            = new SAXParseable<Pattern, NameClass, Locator, VoidValue, CommentListImpl, AnnotationsImpl>(new SAXSource(saxResolver.createXMLReader(), inputSource),
+                                                                                                         saxResolver,
+                                                                                                         eh);
     try {
       return new SchemaImpl(SchemaBuilderImpl.parse(parseable, eh, dlf, spb, false), spb);
     }
