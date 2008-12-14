235ad5b06af45b6b01fe2d39fb2d65a630bf8b64
James Clark <jjc@jclark.com>
2008-12-14 04:15:00 +0000

    Parameterize jing.bat

--- jing.bat
+++ jing.bat
@@ -1,16 +1,18 @@
 @echo off
 setlocal
+set MAIN_CLASS=com.thaiopensource.relaxng.util.Driver
+set JAR_FILE=jing.jar
 set JRE_KEY=HKLM\SOFTWARE\JavaSoft\Java Runtime Environment
 if not exist "%JAVA_HOME%\bin\java.exe" (
-  for /f "tokens=2* delims=	 " %%u in ('reg query "%JRE_KEY%" /v CurrentVersion') do for /f "tokens=2* delims=	 " %%i in ('reg query "%JRE_KEY%\%%v" /v JavaHome') do set JAVA_HOME=%%j
+  for /f "tokens=2*" %%u in ('reg query "%JRE_KEY%" /v CurrentVersion') do for /f "tokens=2*" %%i in ('reg query "%JRE_KEY%\%%v" /v JavaHome') do set JAVA_HOME=%%j
 ) 2>nul
 if exist "%JAVA_HOME%\bin\java.exe" goto found_java
 echo Could not find a Java Runtime Environment. Download one from <http://java.sun.com/javase/downloads/>.
 exit /b 1
 :found_java
 set JAR_DIR=%~dp0
-if exist "%JAR_DIR%jing.jar" goto found_jing
-echo Could not find jing.jar. Must be in the same directory as jing.bat (%JAR_DIR%).
+if exist "%JAR_DIR%%JAR_FILE%" goto found_jar
+echo Could not find %JAR_FILE%. Must be in the same directory as %~nx0 (%JAR_DIR%).
 exit /b 1
-:found_jing
-"%JAVA_HOME%\bin\java.exe" -classpath "%JAR_DIR%jing.jar" com.thaiopensource.relaxng.util.Driver %*
+:found_jar
+"%JAVA_HOME%\bin\java.exe" -classpath "%JAR_DIR%%JAR_FILE%" %MAIN_CLASS% %*
