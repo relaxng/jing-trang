e35062a2e0b5cdbfb59bef353d0bb1c49696a290
Norman Walsh <ndw@nwalsh.com>
2018-10-31 09:05:23 -0500

    Attempt to add support for xs:untypedAtomic and xs:anyAtomicType to XMLSchema-datatypes

---
+++ mod/xsd-datatype/src/main/com/thaiopensource/datatype/xsd/AnyAtomicTypeDatatype.java
@@ -0,0 +1,7 @@
+package com.thaiopensource.datatype.xsd;
+
+class AnyAtomicTypeDatatype extends TokenDatatype {
+  AnyAtomicTypeDatatype() {
+    super(WHITE_SPACE_PRESERVE);
+  }
+}
---
+++ mod/xsd-datatype/src/main/com/thaiopensource/datatype/xsd/UntypedAtomicDatatype.java
@@ -0,0 +1,7 @@
+package com.thaiopensource.datatype.xsd;
+
+class UntypedAtomicDatatype extends TokenDatatype {
+  UntypedAtomicDatatype() {
+    super(WHITE_SPACE_PRESERVE);
+  }
+}
